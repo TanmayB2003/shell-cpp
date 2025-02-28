f6788c2d5beda350b29c6156e9cf5e13727ec073
TanmayB2003 <tanmaybhatnagar@outlook.com>
2025-02-28 23:04:21 +0530

    add repl

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,16 @@
 
 int main() {
   // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+  while(1) {
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
-  std::cout << "$ ";
+    // Uncomment this block to pass the first stage
+    std::cout << "$ ";
 
-  std::string input;
-  std::getline(std::cin, input);
+    std::string input;
+    std::getline(std::cin, input);
 
-  std::cout << input << ": command not found" << std::endl;
+    std::cout << input << ": command not found" << std::endl;
+  }
 }
