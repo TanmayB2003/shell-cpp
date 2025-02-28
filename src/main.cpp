9a7386cdbadb7ed345680e95f2115ba1146ba695
TanmayB2003 <tanmaybhatnagar@outlook.com>
2025-02-28 23:47:12 +0530

    add type

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <sstream>
-// #include <string>
+#include <string>
 #include <vector>
+#include <unordered_set>
 
 int main() {
   // Flush after every std::cout / std:cerr
+  std::unordered_set<std::string> builtin({"type", "exit", "echo"});
   while(1) {
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
@@ -15,10 +17,19 @@ int main() {
     std::string input;
     std::getline(std::cin, input);
    
-    if(input == "exit 0") return 0;
+    if(input.substr(0, 4) == "exit") return std::stoi(input.substr(5));
 
     if(input.substr(0, 4) == "echo") {
       std::cout << input.substr(5) << std::endl; 
+      continue;
+    }
+
+    if(input.substr(0, 4) == "type") {
+      std::string s = input.substr(5);
+      if(builtin.find(s)!=builtin.end()) 
+        std::cout << s << " is a shell builtin" << std::endl;
+      else 
+        std::cout << s << ": not found" << std::endl;
       continue;
     }
 
