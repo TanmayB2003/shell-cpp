0179d60edf074a7018faea229b5290a04fd4e13b
TanmayB2003 <tanmaybhatnagar@outlook.com>
2025-03-01 00:29:00 +0530

    add type executable

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,29 @@
 #include <string>
 #include <vector>
 #include <unordered_set>
+#include <unordered_map>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+std::string get_path(std::string &command) {
+  const std::string path_env = std::getenv("PATH");
+  std::stringstream ss(path_env);
+  std::string path;
+  while(getline(ss,path,':')) {
+    std::string abs_path = path + '/' + command;
+    if(fs::exists(abs_path)) {
+      return abs_path;
+    }
+  }
+  return "";
+}
 
 int main() {
   // Flush after every std::cout / std:cerr
   std::unordered_set<std::string> builtin({"type", "exit", "echo"});
+  
+
   while(1) {
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
@@ -26,8 +45,12 @@ int main() {
 
     if(input.substr(0, 4) == "type") {
       std::string s = input.substr(5);
+      std::string exec = get_path(s);
+
       if(builtin.find(s)!=builtin.end()) 
         std::cout << s << " is a shell builtin" << std::endl;
+      else if(exec!="") 
+        std::cout << s << " is " << exec << std::endl;
       else 
         std::cout << s << ": not found" << std::endl;
       continue;
